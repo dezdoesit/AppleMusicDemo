7ba39d1b411846d3bc97431e10721f3df09329e7
dezdoesit <dez.blair@icloud.com>
2023-11-22 14:20:22 -0500

    Initial Commit

---
+++ AppleMusicDemo/AppleMusicDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicDemoApp.swift
+//  AppleMusicDemo
+//
+//  Created by dmoney on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
